4130ec81be5864ce937ab86ad7f04da57a468166
Lucas Quintanilla <aurorawwr@gmail.com>
2025-10-13 16:00:21 -0300

    ADD: Avances en perfil del usuario

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,41 @@
+import { useEffect, useState } from "react";
+import { getUserProfile } from "../services/user";
+import { useNavigate } from "react-router";
+import { ROUTE_PATHS } from "../utils/constants";
+import Loading from "../components/Loading";
+import { Col, Image, Row } from "react-bootstrap";
+
 function Profile() {
+  const navigate = useNavigate();
+  const [userInfo, setUserInfo] = useState(undefined);
+
+  useEffect(() => {
+    getUserProfile()
+    .then((user) => {
+        setUserInfo(user);
+      })
+      .catch((err) => {
+        console.error(err.message);
+        navigate(ROUTE_PATHS.HOME);
+      });
+  }, []);
+
+  if (userInfo == undefined) {
+    return <Loading />;
+  }
 
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <Row>
+      <Col xs={12} sm={8}>
+        <div>
+          <Image className="object-fit-cover" src={userInfo.profilePicture} width={100}  height={100} roundedCircle />
+        </div>
+      </Col>
+      <Col xs={12} sm={4} className="text-center">
+        <h5>Nivel de Estim</h5>
+        <h1>{userInfo.profileLevel}</h1>
+      </Col>
+    </Row>
   )
 }
 
